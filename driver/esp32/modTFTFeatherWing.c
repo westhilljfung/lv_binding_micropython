bee74f7090c87578ba506661cc7bc3bcc07730c4
Joshua Fung <jfung@westhillinnvation.com>
2019-07-15 14:54:35 -0400

    isolate stmpe driver in

--- driver/esp32/modTFTFeatherWing.c
+++ driver/esp32/modTFTFeatherWing.c
@@ -316,7 +316,7 @@ STATIC mp_obj_t mp_init_TFTFeatherWing(mp_obj_t self_in) {
 
    //Attach the Touch Screen to the SPI bus
    spi_device_interface_config_t devcfg_ts={
-      .clock_speed_hz=1000000, //Clock out at 1 MHz
+      .clock_speed_hz=4000, //Clock out at 1 MHz
       .mode=0,                             //SPI mode 0
       .spics_io_num=32,              //CS pin
       .queue_size=1,
