d294d6a00f5032b0119d8446d4e320cbbdb574c4
Joshua Fung <jfung@westhillinnvation.com>
2019-07-22 13:14:22 -0400

    changed dma channel

--- driver/esp32/modTFTFeatherWing.c
+++ driver/esp32/modTFTFeatherWing.c
@@ -503,7 +503,7 @@ STATIC void spi_bus_device_init(TFTFeatherWing_obj_t *self) {
       .max_transfer_sz=128*1024,
    };
    
-   ret = spi_bus_initialize(self->spihost, &buscfg, 1);
+   ret = spi_bus_initialize(self->spihost, &buscfg, 2);
    
    if (ret != ESP_OK) {
       // Assume bus is taken but in the right channels
